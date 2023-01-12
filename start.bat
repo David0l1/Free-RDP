ff95a353ce38d684641c993df05f43474a13a925
Onee-Dev <107624642+David0l1@users.noreply.github.com>
2023-01-12 18:40:50 +0700

    Asu

--- start.bat
+++ start.bat
@@ -17,7 +17,7 @@ echo IP :
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Masukkan IP RDP tanpa tcp://
 echo Enter RDP IP without tcp://
-echo 『 RDP Account 』
+echo > RDP Account 
 echo Username: Administrator
 echo Password: Onee-Dev
 echo ===== END =====
